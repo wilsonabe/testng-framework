879cb2abdd30f1c763332354a5079d61573a7b0f
wilsonabe <wilson.abe@gmail.com>
2020-09-26 18:14:03 -0400

    Create Login test. Closes #2

---
+++ src/test/java/com/orphynova/demo/LoginTests.java
@@ -0,0 +1,31 @@
+package com.orphynova.demo;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTests {
+
+    @Test(priority = 1,testName = "VALID Credentials Login Test")
+    public void logintest(){
+        System.out.println("Login with valid user name and password");
+
+        String exp = "Hello";
+        String act = "Hello";
+        Assert.assertEquals(exp,act,"The strings dont match");
+    }
+    @Test(priority = 2,testName = "INVALID Credentials Login Test")
+    public void invalidLoginTest(){
+        System.out.println("Login test with invalid user name and password unsuccessful");
+    }
+
+    @Test(priority = 3,testName = "Verify Home page")
+    public void homePageTest(){
+        System.out.println("Home page test ");
+    }
+
+    @Test(priority = 4,testName = "Test for logout")
+    public void logOutTest(){
+        System.out.println("Logout test");
+    }
+
+}
